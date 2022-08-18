28344c3d50378376e5171b3847409d8ced20f614
berkagmpp <berkagmpp@gmail.com>
2022-08-18 13:26:01 +1200

    init

---
+++ custom.js
@@ -0,0 +1,13 @@
+'use strict'
+
+// const tabA = document.getElementsByClassName("tab-a");
+// const tabB = document.getElementsByClassName("tab-b");
+// const tabC = document.getElementsByClassName("tab-c");
+// const tabD = document.getElementsByClassName("tab-d");
+// const tabE = document.getElementsByClassName("tab-e");
+// const tabF = document.getElementsByClassName("tab-f");
+
+// tabA.onclick = () => {
+//     tabA.classList.add("actived");
+//     tabB.classList.remove("actived");
+// }
